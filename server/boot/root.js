4894a15b7f8d81049fce19664dad4580e3c9fcea
Adron Hall <adronhall@gmail.com>
2015-08-10 08:18:36 -0700

    Added test route.

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,5 @@
-module.exports = function (app) {
-  // Install a "/ping" route that returns "pong"
-  app.get('/ping', function(req, res) {
-    res.send('pong');
-  });
+module.exports = function (server) {
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  server.use(router);
 };
